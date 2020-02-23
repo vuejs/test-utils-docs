de1af28cb0dc8ad050b59d7f6324bf1ef33cfa94
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-02-23 10:53:56 +1000

    init with vuepress

---
+++ .vuepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  base: '/vue-test-utils-next/',
+  title: 'Vue Test Utils',
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: 'Vue Test Utils',
+    },
+  },
+  themeConfig: {
+    editLinks: true,
+    sidebarDepth: 2,
+    locales: {
+      '/': {
+        label: 'English',
+        selectText: 'Languages',
+        lastUpdated: 'Last Updated',
+        editLinkText: 'Edit this page on GitHub',
+        sidebar: [
+          '/',
+          '/guides/',
+          '/api-reference/',
+        ],
+      },
+    },
+  },
+}
+
---
+++ guides/README.md
@@ -0,0 +1,3 @@
+# Guides
+
+Todo: Write useful guides. See [this issue](https://github.com/lmiller1990/vue-testing-framework/issues/11) for some guidance and inspiration. Remember, we are not just teaching people "this is how to use Vue Test Utils" but "this is how to test your Vue.js components".
