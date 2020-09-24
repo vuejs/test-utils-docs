88b39642eb503dd2cda7031cb181a9c8f750bc09
Jessica Sachs <jess@jessicasachs.io>
2020-09-23 21:15:34 -0400

    chore: cleaning up build script and package.json

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 rm -rf tmp
 rm -rf src/.vuepress/dist
-yarn build-local
-mv src/.vuepress/dist tmp
+
+yarn build:dev
+
+# Nesting dist into a subdirectory inside itself dist/v2
 mv src/.vuepress/dist tmp
 mkdir -p src/.vuepress/dist/v2
 mv tmp/* src/.vuepress/dist/v2
+
+# Pulling up the top-level pages
 cp src/.vuepress/dist/v2/*.html src/.vuepress/dist/
